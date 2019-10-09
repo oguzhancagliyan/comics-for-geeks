303c9966fc817146f5be2ea55a471e89fc276a37
Oğuzhan Çağlıyan <44145788+oguzhancagliyan@users.noreply.github.com>
2019-10-09 17:33:22 +0300

    mongo-base-implementation
    base implementation completed

---
+++ Proletarians/Services/Mongo/IMongoDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proletarians.Services.Mongo
+{
+    public interface IMongoDB<T>
+    {
+        #region Async
+
+        /// <summary>
+        /// Get one result from an id
+        /// </summary>
+        /// <typeparam name="T">Generic type</typeparam>
+        /// <param name="id">Id</param>
+        /// <returns>T result</returns>
+        Task<T> GetAsync(string id);
+        /// <summary>
+        /// Get all list from MongoDb via async
+        /// </summary>
+        /// <typeparam name="T">Generic type</typeparam>
+        /// <returns>Return all element from DB</returns>
+        Task<List<T>> GetAllAsync();
+        Task CreateAsync(T instance);
+        Task CreateManyAsync(List<T> createList);
+        Task UpdateAsync(string id, T instance);
+        Task UpdateManyAsync(IDictionary<string, T> updateList);
+        Task DeleteAsync(T instance);
+        Task DeleteManyAsync(List<T> deleteList);
+
+        Task<List<T>> GetByConditionAsnyc(Expression<Func<T, bool>> expression);
+        #endregion
+    }
+}
